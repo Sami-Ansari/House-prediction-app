96cb3619309c8fedaa777cf2588aa188d6aaef96
Sami-Ansari <samiansari8800@gmail.com>
2023-12-25 19:51:23 +0500

    commit

--- boston-house.py
+++ boston-house.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
-
-# import sharp
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
 
+st.set_page_config("Prediction APP")
 
 st.write(
     """
@@ -84,7 +83,7 @@ st.header("Specified Input Parameters")
 st.write(df)
 st.write("---")
 
-    # Build Regression Model
+ # Build Regression Model
 model = RandomForestRegressor()
 model.fit(X, Y)
  # Apply Model to make predicion
@@ -93,3 +92,18 @@ prediction = model.predict(df)
 st.header("Predictionof MEDV")
 st.write(prediction)
 st.write("---")
+
+# Explaining the model's predictions using SHAP values
+# https://github.com/slundberg/shap
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st.header('Feature Importance')
+plt.title('Feature importance based on SHAP values')
+shap.summary_plot(shap_values, X)
+st.pyplot(bbox_inches='tight')
+st.write('---')
+
+plt.title('Feature importance based on SHAP values (Bar)')
+shap.summary_plot(shap_values, X, plot_type="bar")
+st.pyplot(bbox_inches='tight')

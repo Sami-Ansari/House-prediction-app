d7db0cb4c61812adf30bf5a730172f66f76b3235
Sami-Ansari <samiansari8800@gmail.com>
2023-12-25 19:54:26 +0500

    commit

--- boston-house.py
+++ boston-house.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
 
-st.set_page_config("Prediction APP")
+st.set_page_config("House Price")
 
 st.write(
     """
